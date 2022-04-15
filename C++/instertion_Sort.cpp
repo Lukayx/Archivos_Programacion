b499c5235b6c822c2d040503636d82c598bbe3c8
Ferinz-o <olavarriafernando36@gmail.com>
2022-04-15 01:17:03 -0400

    varios cambios y extenciones agregados

--- C++/instertion_Sort.cpp
+++ C++/instertion_Sort.cpp
@@ -1,31 +1,35 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <time.h>
 using namespace std;	// uso del espacio de nombre std
 
-void insertionSort(int &A,int n){
+void insertionSort(int *A,int n){
     int j,key;
     for(int i = 2; i < n;i++){
         key = A[i];
         j = i - 1;
         while(j > 0 && A[j] > key){
-            A[j+1] = a[j];
+            A[j+1] = A[j];
             j--;
             A[j+1]=key;
         }
     }
 }
 int main(){
+    srand(time(NULL));
     int n;
-    cout << "Ingrese el tamaño de la lista: ";
+    cout << "Ingrese el largo de la lista: ";
     cin >> n;
-    int *lista[] = new int[n];
+    int *lista = new int[n];
     for(int i = 0; i<n;i++){
         lista[i] = rand()%100;
+        cout << lista[i] << " ";
     }
+    cout << endl;
     insertionSort(lista,n);
     for(int j = 0;j<n ;j++){
-        cout << " " << lista[j];
+        cout << lista[j] << " ";
     }
     return 0;
 }
