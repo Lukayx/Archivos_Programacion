7f16809c8f0a0365e44127bef76ebd46ff8a0a23
Lukayx <olavarriafernando36@gmail.com>
2022-04-19 11:42:29 -0400

    InsertionSort Ascendente, Decendente y Mixto

--- C++/instertion_Sort.cpp
+++ C++/instertion_Sort.cpp
@@ -1,31 +1,51 @@
 #include <iostream>
 #include <cstring>
 #include <cstdlib>
+#include <time.h>
 using namespace std;	// uso del espacio de nombre std
 
-void insertionSort(int &A,int n){
-    int j,key;
-    for(int i = 2; i < n;i++){
-        key = A[i];
-        j = i - 1;
-        while(j > 0 && A[j] > key){
-            A[j+1] = a[j];
-            j--;
-            A[j+1]=key;
-        }
-    }
-}
+void insertionSort(int* A,int n);
+void insertionSortDesc(int* A, int n);
+
 int main(){
-    int n;
-    cout << "Ingrese el tamaño de la lista: ";
+    srand(time(NULL));
+    int n, *lista;
+    cout << "Ingrese el largo de la lista: ";
     cin >> n;
-    int *lista[] = new int[n];
+    lista = new int[n];
     for(int i = 0; i<n;i++){
-        lista[i] = rand()%100;
+        lista[i] = rand()%101;
     }
     insertionSort(lista,n);
+    //insertionSortDesc(lista,n);
     for(int j = 0;j<n ;j++){
         cout << " " << lista[j];
     }
     return 0;
+}
+
+void insertionSort(int* A,int n){
+    int j,key;
+    for(int i = 1; i < n;i++){
+        key = A[i];
+        j = i - 1;
+        while(j >= 0 && A[j] > key){
+            A[j+1] = A[j];
+            j = j - 1;
+        }
+        A[j+1]=key;
+    }
+}
+
+void insertionSortDesc(int* A, int n){
+    int j,key;
+    for(int i = 1; i < n;i++){
+        key = A[i];
+        j = i - 1;
+        while(j >= 0 && A[j] < key){
+            A[j+1] = A[j];
+            j = j - 1;
+        }
+        A[j+1]=key;
+    }
 }
