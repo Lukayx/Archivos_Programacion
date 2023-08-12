fee3ff72f54bd7c7ea73f58ff96576e58f4c9ede
Lukayx <olavarriafernando36@gmail.com>
2023-08-12 01:39:20 -0400

    Comenze con ramo de Sistemas operativos

---
+++ C++/2023/Sistemas operativos/trabajo1.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+int getout(string u, int v);
+  
+int main(){
+  ifstream archivo("db.txt");
+
+  
+}
+
+int getout(string u, int v){
+
+}
